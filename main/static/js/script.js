09a11482b1715ed26c7424c8dd1dbd20403f82ef
maaaksoon <maaaksoon@gmail.com>
2023-07-25 21:43:23 +0500

    добавил кнопку и картинку

---
+++ main/static/js/script.js
@@ -0,0 +1,9 @@
+function openModal() {
+  var modal = document.getElementById("photoModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("photoModal");
+  modal.style.display = "none";
+}
